af99bf9e2bea4143fe3a390e9fad8bdb5b682957
alecarones <66912467+alecarones@users.noreply.github.com>
2025-06-04 15:27:45 +0200

    Delete costi directory

--- costi/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._compsep import (
-    component_separation, estimate_residuals
-)
-from .simulations import _get_data_foregrounds_, _get_data_simulations_, _get_noise_simulation, _get_cmb_simulation, _get_foregrounds
-from .configurations import Configs, get_params
-from .routines import (_get_nside_lmax_from_b_ell)
