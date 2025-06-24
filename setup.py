4e557ead0c41ab3f4bff418c5f7043092ac5e27c
alecarones <66912467+alecarones@users.noreply.github.com>
2025-06-24 14:20:40 +0200

    Adding setup files

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="broom",
+    version="0.1.0",
+    description="A Python package for blind component separation of microwave sky maps.",
+    author="Alessandro Carones",
+    author_email="acarones@sissa.it",
+    url="https://github.com/alecarones/broom",
+    project_urls={
+        "Homepage": "https://github.com/alecarones/broom",
+        "Source": "https://github.com/alecarones/broom",
+        "Issues": "https://github.com/alecarones/broom/issues",
+    },
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    license="GPL-3.0-or-later",
+    python_requires=">=3.8",
+    packages=find_packages(where="."),
+    install_requires=[
+        "numpy>=1.18.5",
+        "scipy>=1.8",
+        "healpy>=1.15",
+        "pysm3>=3.3.2",
+        "mtneedlet>=0.0.5",
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
