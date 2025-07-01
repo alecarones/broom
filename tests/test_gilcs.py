c22dbba6b50be126cae63172d1e6960ff0737757
Giacomo Galloni <giacomo.galloni@unife.it>
2025-07-01 07:56:53 +0200

    Update test_gilcs.py

--- tests/test_gilcs.py
+++ tests/test_gilcs.py
@@ -5,6 +5,6 @@ def test_import_gilcs():
     assert hasattr(broom.gilcs, "__file__")
 
 
-def test_gilc_runs():
+def test_gilc_runs(config_all_path):
     # Provide minimal dummy arguments
     broom.gilcs.gilc(None, None, None)
