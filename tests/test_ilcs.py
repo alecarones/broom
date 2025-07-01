0933313741ffae4b4c110aa2f50929bdaaa55d41
Giacomo Galloni <giacomo.galloni@unife.it>
2025-07-01 09:16:08 +0200

    Add `cilc` tests

--- tests/test_ilcs.py
+++ tests/test_ilcs.py
@@ -29,3 +29,22 @@ def test_ilc_runs(config_all_path):
             compsep["needlet_config"] = merge_dicts(compsep["needlet_config"])
         config.compsep = [compsep]
         broom.ilcs.ilc(config, input_alms, compsep)
+
+def test_cilc_runs(config_all_path):
+    config = Configs(config_all_path)
+    compseps = config.compsep
+    selected_compseps = [compsep for compsep in compseps if compsep["method"] == "cilc" or compsep["method"] == "c_ilc"]
+
+    rng = np.random.default_rng()
+    input_alms = SimpleNamespace(
+        total=1e-6
+        * rng.random((len(config.instrument.frequency), 3, hp.Alm.getsize(config.lmax)))
+        * (1 + 1j)
+    )
+
+    for compsep in selected_compseps:
+        if compsep["domain"] == "needlet":
+            compsep["needlet_config"] = merge_dicts(compsep["needlet_config"])
+        compsep["constraints"] = merge_dicts(compsep["constraints"])
+        config.compsep = [compsep]
+        broom.ilcs.ilc(config, input_alms, compsep)
