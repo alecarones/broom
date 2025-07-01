67309556bc550820baf31eab1ffde66a9bb32ece
Giacomo Galloni <giacomo.galloni@unife.it>
2025-07-01 10:15:35 +0200

    Add `routines` tests

--- tests/test_routines.py
+++ tests/test_routines.py
@@ -1,12 +1,24 @@
-import broom.routines
+from types import SimpleNamespace
+
+import healpy as hp
 import numpy as np
+
+import broom.routines
+from broom.configurations import Configs
 
 
 def test_import_routines():
     assert hasattr(broom.routines, "__file__")
 
 
-def test_get_local_cov_runs():
-    # Provide minimal dummy arguments
-    arr = np.ones((2, 10))
-    broom.routines._get_local_cov(arr, 8, 0.0)
+def test_get_local_cov_runs(config_simple_path):
+    config = Configs(config_simple_path)
+
+    rng = np.random.default_rng()
+    input_alms = SimpleNamespace(total=1e-6 * rng.random((3, 12 * config.nside**2)))
+
+    broom.routines._get_local_cov(
+        input_maps=input_alms.total,
+        lmax=config.lmax,
+        ilc_bias=config.compsep[0]["ilc_bias"],
+    )
