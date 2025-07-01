85f8a4f438a5877d34312865d5f5be9863b4951e
Giacomo Galloni <giacomo.galloni@unife.it>
2025-07-01 10:07:17 +0200

    Fix `needlets` tests

--- tests/test_needlets.py
+++ tests/test_needlets.py
@@ -1,12 +1,19 @@
+from broom.configurations import Configs
 import broom.needlets
+from broom.routines import merge_dicts
 
 
 def test_import_needlets():
     assert hasattr(broom.needlets, "__file__")
 
 
-if hasattr(broom.needlets, "_get_needlet_windows_"):
+def test_get_needlet_windows_runs(config_all_path):
+    config = Configs(config_all_path)
+    compseps = config.compsep
+    selected_compseps = [compsep for compsep in compseps if "needlet_config" in compsep]
 
-    def test_get_needlet_windows_runs():
-        # Provide minimal dummy arguments
-        broom.needlets._get_needlet_windows_({}, 8)
+    for compsep in selected_compseps:
+        compsep["needlet_config"] = merge_dicts(compsep["needlet_config"])
+        broom.needlets._get_needlet_windows_(
+            compsep["needlet_config"], config.lmax
+        )
