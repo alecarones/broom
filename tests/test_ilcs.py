cd9c5d0f7515a3ae24c43b0b1799c8e9e10bcc49
Giacomo Galloni <giacomo.galloni@unife.it>
2025-07-01 07:26:51 +0200

    Fix `ilcs` tests

--- tests/test_ilcs.py
+++ tests/test_ilcs.py
@@ -1,10 +1,30 @@
+from types import SimpleNamespace
+import numpy as np
+import healpy as hp
+from broom.configurations import Configs
 import broom.ilcs
+from broom.routines import merge_dicts
 
 
 def test_import_ilcs():
     assert hasattr(broom.ilcs, "__file__")
 
 
-def test_ilc_runs():
-    # Provide minimal dummy arguments
-    broom.ilcs.ilc(None, None, None)
+def test_ilc_runs(config_all_path):
+    config = Configs(config_all_path)
+    compseps = config.compsep
+    selected_compseps = [compsep for compsep in compseps if compsep["method"] == "ilc"]
+
+    rng = np.random.default_rng()
+    input_alms = SimpleNamespace(
+        total=1e-6
+        * rng.random((len(config.instrument.frequency), 3, hp.Alm.getsize(config.lmax)))
+        * (1 + 1j)
+    )
+
+    for compsep in selected_compseps:
+        if compsep["domain"] == "needlet":
+            compsep["needlet_config"] = merge_dicts(compsep["needlet_config"])
+        config.compsep = [compsep]
+        print(config.compsep)
+        broom.ilcs.ilc(config, input_alms, compsep)
