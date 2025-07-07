b70f8ee9487383d33f0c76918a7e879ec38412e0
Anto Idicherian Lonappan <antolonappan@icloud.com>
2025-07-06 17:29:27 -0700

    pandas

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "pysm3>=3.3.2",
         "mtneedlet>=0.0.5",
         "threadpoolctl>=3.6.0",
-        "pandas>=2.3.0
+        "pandas>=2.3.0"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
